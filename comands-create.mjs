6557a119ee7c6c867c5411378d43dcd860bf698e
Vladimir Noutbook <vovirus2@inbox.ru>
2025-05-12 16:52:22 +0300

    practice fs

--- comands-create.mjs
+++ comands-create.mjs
@@ -1,12 +1,24 @@
+import fs from 'fs';
+import path from 'path';
 if (!process.argv[2] || !process.argv[3]) {
     console.log('need 3 args or 4');
     process.exit();
 }
 const fileSecond = process.argv[2];
-
+const lineQty = process.argv[3];
 const fileThird = parseInt(process.argv[3]);
 if (isNaN(fileThird)) {
     console.log('Nan');
     process.exit();
 }
-console.log(fileThird);
+console.log('start', performance.now());
+const writeStream = fs.createWriteStream(path.join('./files', fileSecond));
+
+for (let i = 0; i < lineQty; i++) {
+    writeStream.write(`This is ${i} line\n`);
+}
+writeStream.end(() => console.log('write completed'));
+console.log('End', performance.now());
+setTimeout(() => {
+    console.log('TimeOut', performance.now());
+}, 0);
