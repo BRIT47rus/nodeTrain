eebb9fb44d03692139e69506b037c245d8295769
Vladimir Noutbook <vovirus2@inbox.ru>
2025-05-12 11:17:35 +0300

    transform

--- index.mjs
+++ index.mjs
@@ -1,6 +1,14 @@
-import stream from 'stream';
+import { Transform } from 'stream';
 import fs from 'fs';
-const text = './files/text.txt';
+// const text = './files/text.txt';
 
-const writeble = fs.createWriteStream(text);
-process.stdin.pipe(writeble);
+// const writeble = fs.createWriteStream(text);
+// process.stdin.pipe(writeble);
+
+const upper = new Transform({
+    transform: (chunk, encoding, cb) => {
+        const upperCase = chunk.toString().toUpperCase();
+        cb(null, upperCase);
+    },
+});
+process.stdin.pipe(upper).pipe(process.stdout);
