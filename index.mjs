96efa8cf3d92dc2420623d48f1bb6bedb7a5056d
Vladimir Noutbook <vovirus2@inbox.ru>
2025-05-12 11:03:15 +0300

    pipe

--- index.mjs
+++ index.mjs
@@ -1,29 +1,6 @@
-import http from 'http';
+import stream from 'stream';
 import fs from 'fs';
-import { log } from 'console';
-const PORT = 5000;
+const text = './files/text.txt';
 
-const server = http.createServer((req, res) => {
-    const file = './streams/files/http-stream.html';
-    if (req.url === '/' && req.method === 'GET') {
-        const readStream = fs.createReadStream(file);
-        res.statusCode = 200;
-        res.setHeader('Content-type', 'text/html');
-        readStream.pipe(res);
-    }
-    if (req.url === '/no-stream' && req.method === 'GET') {
-        fs.readFile('./streams/files/http-nostream.html', (err, data) => {
-            if (err) {
-                res.statusCode = 500;
-                log(err);
-            } else {
-                res.statusCode = 200;
-                res.setHeader('Content-type', 'text/html');
-                res.end(data);
-            }
-        });
-    }
-});
-server.listen(PORT, () => {
-    log('server starten on ', PORT);
-});
+const writeble = fs.createWriteStream(text);
+process.stdin.pipe(writeble);
